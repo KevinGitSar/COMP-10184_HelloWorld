117da2664cc25138d7e5662a4da7f6ddac53c576
KevinGitSar <kevinsar.97@gmail.com>
2022-09-12 16:59:39 -0400

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  // start the USB serial monitor port
+  Serial.begin(115200);
+
+  // print some text to the USB port
+  Serial.println("\n\nAuthor: Kevin Sar");
+  Serial.print("ESP8266 Chip ID: ");
+  Serial.println(ESP.getChipId());
+  Serial.print("Flash Chip ID: ");
+  Serial.println(ESP.getFlashChipId());
+  Serial.println("\n\nRunning Time in Milliseconds every 2 seconds: ");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+  Serial.println();
+  Serial.print(millis());
+  Serial.println(" ms");
+}
